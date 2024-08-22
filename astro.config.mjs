594f6136f6ef5821a240c7ba8cd3ea324156afe7
Tona González <hi@tona.dev>
2024-08-22 13:07:59 -0600

    feat: use nodejs standalone

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: node({
+    mode: "standalone",
+  }),
+});
