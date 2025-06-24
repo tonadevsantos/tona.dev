c46443102d6bb17df0bc99d3962c09a803c92251
Tona González <hi@tona.dev>
2025-06-24 15:23:47 -0600

    Use tailwind and change landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: node({
-    mode: "standalone"
+    mode: "standalone",
   }),
   output: "server",
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
---
+++ src/content/links.ts
@@ -0,0 +1,23 @@
+export const links = [
+  {
+    name: "GitHub",
+    url: "https://github.com/tonadevsantos",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/tonadev",
+  },
+  {
+    name: "dev.to",
+    url: "https://dev.to/tonadev",
+  },
+  {
+    name: "X",
+    url: "https://x.com/tonadev",
+  },
+];
+
+export const prettyParams = {
+  firefox: "https://addons.mozilla.org/en-US/firefox/addon/pretty-params/",
+  edge: "https://microsoftedge.microsoft.com/addons/detail/pretty-params/fiddpabcckahhdlepgbbmmenophlkaje",
+};
